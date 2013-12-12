cabbf929bc6b3031183915336abbf0191befdb5d
niquola <niquola@gmail.com>
2013-12-13 02:20:52 +0400

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fp_patterns
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/fp_patterns/core_test.clj
@@ -0,0 +1,7 @@
+(ns fp-patterns.core-test
+  (:require [clojure.test :refer :all]
+            [fp-patterns.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
